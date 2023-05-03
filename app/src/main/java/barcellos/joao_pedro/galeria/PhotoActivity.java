32f3427f2c25b77589d8eafd7dc249bb9e54ded9
20211tiimi0087 <jpzbarcellos@gmail.com>
2023-05-03 12:51:55 -0300

    Primeiro Commit

--- app/src/main/java/barcellos/joao_pedro/galeria/PhotoActivity.java
+++ app/src/main/java/barcellos/joao_pedro/galeria/PhotoActivity.java
@@ -5,18 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+public class PhotoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_photo);
 
         // Pegando a toolbar do MainActivity
         Toolbar toolbar = findViewById(R.id.tbMain);
         setSupportActionBar(toolbar);
         // Setando a toolbar como padrão da MainActivity
-
-
     }
 }
