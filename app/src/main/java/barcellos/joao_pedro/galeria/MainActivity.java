f2def663f173d3872e0bebfa08a3035aae06b76d
20211tiimi0087 <jpzbarcellos@gmail.com>
2023-05-10 12:56:47 -0300

    Terceiro Commit

--- app/src/main/java/barcellos/joao_pedro/galeria/MainActivity.java
+++ app/src/main/java/barcellos/joao_pedro/galeria/MainActivity.java
@@ -3,13 +3,26 @@ package barcellos.joao_pedro.galeria;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    List<String> photos = new ArrayList<>();
+    // Lista com todos os URI's das fotos
+
+
+    MainAdapter mainAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +33,29 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.tbMain);
         setSupportActionBar(toolbar);
         // Setando a toolbar como padrão da MainActivity
+
+        // Acessando o diretorório
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File[] files = dir.listFiles();
+        // Pegando todas as fotos já salvas anteriormente e adicionando na lista de fotos
+        for(int i = 0; i < files.length; i++){
+            photos.add(files[i].getAbsolutePath());
+        }
+
+        mainAdapter = new MainAdapter(MainActivity.this, photos);
+        // Pegando o MainAdapter
+
+        // Pegando o RecyclerView da MainActivity e setando nele o MainAdapter
+        RecyclerView rvGallery = findViewById(R.id.rvGallery);
+        rvGallery.setAdapter(mainAdapter);
+
+        // Calculando quantas columas cabem na tela
+        float w = getResources().getDimension(R.dimen.itemWidth);
+        int numberOfColumns = Util.calculateNoOfColumns(    MainActivity.this, w);
+
+        // Criando o gridLayout e setando no RecycleView
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(MainActivity.this, numberOfColumns);
+        rvGallery.setLayoutManager(gridLayoutManager);
     }
     public boolean onCreateOptionsMenu(Menu menu){
         super.onCreateOptionsMenu(menu);
---
+++ app/src/main/java/barcellos/joao_pedro/galeria/MainAdapter.java
@@ -0,0 +1,58 @@
+package barcellos.joao_pedro.galeria;
+
+import android.graphics.Bitmap;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MainAdapter extends RecyclerView.Adapter {
+
+    MainActivity mainActivity;
+    List<String> photos; // Lista de strings com os URI's das fotos
+
+    public MainAdapter(MainActivity mainActivity, List<String> photos){
+        this.mainActivity = mainActivity;
+        this.photos = photos;
+    }
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
+        // Pegando o ImageView
+        ImageView imPhoto = holder.itemView.findViewById(R.id.imItem);
+
+        // Colocando na variável w o valor setado no dimen.xml
+        int w = (int) mainActivity.getResources().getDimension(R.dimen.itemWidth);
+
+        // Colocando na variável h o valor setado no dimen.xml
+        int h = (int) mainActivity.getResources().getDimension(R.dimen.itemHeight);
+
+        // Pegando a imagem, colocando o tamanho de w e h e carregando em um bitmap
+        Bitmap bitmap = Util.getBitmap(photos.get(position), w, h);
+
+        // Colocando o Bitmap no ImageView
+        imPhoto.setImageBitmap(bitmap);
+        // Quando o usuário clicar em uma imagem
+        imPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Navegando para a PhotoActivity
+                mainActivity.startPhotoActiviy(photos.get(position));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
